96e2e91b182f9a189261586f2ac94c6332c1d63b
qnchuck <74859195+qnchuck@users.noreply.github.com>
2022-01-11 23:03:16 +0100

    vote counter almost done

--- VoteCounter/VoteCounter.cpp
+++ VoteCounter/VoteCounter.cpp
@@ -16,79 +16,133 @@
 #include <Common/CommonMethods.h>
 
 #pragma comment(lib, "Ws2_32.lib")
+struct ThreadCountVotes {
+    std::deque<Common::Vote> Votes;
+    Common::CountedVotes* Counted;
 
+    explicit ThreadCountVotes(
+        const std::deque<Common::Vote>& votes,
+        Common::CountedVotes* counted
+    ):
+        Votes(votes), Counted(counted){}
 
-Common::CountedVotes countVotesElections(std::deque<Common::Vote> countVotes) {
-  std::deque<Common::Vote> Votes = countVotes;
+    ThreadCountVotes(
+        const ThreadCountVotes& ref
+    ):
+        Votes(ref.Votes), Counted(ref.Counted){};
 
-  Common::CountedVotes counted;
+    ThreadCountVotes() = delete;
 
-  for (size_t i = 0; i < countVotes.size(); i++)
-  {
-    counted.IncreaseVoteNumber(countVotes[i].PartyNumber);
-  }
+    ~ThreadCountVotes() {
+        Votes.clear();
+    }
+};
 
-  return counted;
+DWORD WINAPI Sum(LPVOID lpParam) {
 
+    ThreadCountVotes* countVotes = ((ThreadCountVotes*)lpParam);
+
+    for (size_t i = 0; i < countVotes->Votes.size(); i++)
+    {
+        countVotes->Counted->IncreaseVoteNumber(
+            countVotes->Votes[i].PartyNumber
+        );
+    }
+
+    return 0;
 }
 
+UINT splitNum = 3;
 
 int main()
 {
-  Common::Vote vote1(
-    1, 1
-  ); Common::Vote vote2(
-    2, 3
-  ); Common::Vote vote3(
-    3, 4
-  ); Common::Vote vote4(
-    4, 2
-  ); Common::Vote vote5(
-    5, 2
-  ); Common::Vote vote6(
-    6, 4
-  ); Common::Vote vote7(
-    7, 1
-  ); Common::Vote vote8(
-    8, 1
-  );
+    {
 
-  std::deque<Common::Vote> Votes;
-  Votes.push_back(vote1);
-  Votes.push_back(vote2);
-  Votes.push_back(vote3);
-  Votes.push_back(vote4);
-  Votes.push_back(vote5);
-  Votes.push_back(vote6);
-  Votes.push_back(vote7);
-  Votes.push_back(vote8);
+        Common::CountedVotes countedVotesAll;
+
+        Common::VotingOption opt1("Pera", "asdasd", 1);
+        Common::VotingOption opt2("qweqwe", "asdasd", 2);
+        Common::VotingOption opt3("fgfdg", "asdasd", 3);
+        Common::VotingOption opt4("fgfdg", "asdasd", 4);
+        Common::VotingList votingListReal({
+            opt1, opt2, opt3, opt4
+            });
 
 
+        Common::Vote vote1(
+            1, 1
+        ); Common::Vote vote2(
+            2, 1
+        ); Common::Vote vote3(
+            3, 4
+        ); Common::Vote vote4(
+            4, 2
+        ); Common::Vote vote5(
+            5, 2
+        ); Common::Vote vote6(
+            6, 4
+        ); Common::Vote vote7(
+            7, 1
+        ); Common::Vote vote8(
+            8, 1
+        );
 
-  Common::CountedVotes cnt = countVotesElections(Votes);
-  std::int16_t a = 1;
+
+        std::deque<Common::Vote> Votes;
+        Votes.push_back(vote1);
+        Votes.push_back(vote2);
+        Votes.push_back(vote3);
+        Votes.push_back(vote4);
+        Votes.push_back(vote5);
+        Votes.push_back(vote6);
+        Votes.push_back(vote7);
+        Votes.push_back(vote8);
+
+        Common::VotesContainer toCount(Votes);
+        auto splitted = toCount.GetEquallySeparatedVotes(splitNum);
+
+        std::deque<HANDLE> threadHandles = {};
+        std::deque<ThreadCountVotes> threadParams = {};
 
 
+        for (UINT i = 0; i < splitNum; i++)
+        {
+            threadParams.push_back(ThreadCountVotes(splitted[i], &countedVotesAll));
+
+            HANDLE h = CreateThread(
+                NULL, 0, Sum, &threadParams[i], 0, 0
+            );
+        }
 
 
+        while (threadHandles.size() != 0)
+        {
+            auto it = std::find_if(
+                threadHandles.begin(),
+                threadHandles.end(),
+                [&](HANDLE h) {
+                    if (
+                        WaitForSingleObject(h, 1) == WAIT_OBJECT_0
+                        ) {
+                        CloseHandle(h);
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+            );
+
+            if (it != threadHandles.end()) {
+                threadHandles.erase(it);
+            }
+        }
+
+        countedVotesAll.AsssignZeroVotes(votingListReal.GetOptions());
+        threadHandles.clear();
+        threadParams.clear();
+    }
+    // send
+
+    return 0;
 }
-struct ThreadCountVotes {
-  std::deque<Common::Vote> Votes;
-  Common::CountedVotes* counted;
-};
-
-/*
-DWORD WINAPI Sum(LPVOID aaa) {
-
-  ThreadCountVotes countVotes = ((ThreadCountVotes)aaa);
-
-  std::deque<Common::Vote> Votes = countVotes.Votes;
-  Common::CountedVotes* counted = countVotes.counted;
-
-  for (size_t i = 0; i < countVotes.Votes.size(); i++)
-  {
-    counted->IncreaseVoteNumber(countVotes.Votes[i].PartyNumber);
-  }
-
-  return 0;
-}*/
